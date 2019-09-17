62f0ec2793e12e4587bfd8cd3dd7da2fa2a4ec87
lincoln <hqy@sina.com.cn>
2019-09-17 13:40:19 +0800

    update to v15.2

--- dist/addon/dynamsoft.barcodereader.config.js
+++ dist/addon/dynamsoft.barcodereader.config.js
@@ -6,7 +6,7 @@
  * @preserve Copyright 2019, Dynamsoft Corporation
  * @author Dynamsoft
  *
- * @version 6.5.2
+ * @version 7.1
  *
  * @fileoverview Dynamsoft JavaScript Library for Dynamsoft Barcode Reader
  * More info on DBR: http://www.dynamsoft.com/Products/Dynamic-Barcode-Reader.aspx
@@ -30,7 +30,10 @@ dynamsoft.dbrEnv = dynamsoft.dbrEnv || {};
 //  decompile, disassemble, or modify the productKey .                             //
 
 //////////////////////////////////////////////////////////////////////////////
-dynamsoft.dbrEnv.productKey = 't00901wAAAJ7/4i9Ey29HMzHfov9XhXylM8Mv1vZqg5H29n5IPmx/VNzEXc9exuG6K6ee62ppfXSsIbC0w9XXgOKia+G3QPnv/l+QT8FCaKuhTSmwNhNRsQuyDixP';
+dynamsoft.dbrEnv.productKey = 't00976QAAAJjdYp84Fa5rECnE6HYJmsyjqELLGzjt9LxFJsiiOoSgsVjboNb7GDeQvFXLA2D9vaEykXCnoCYvuEHzYTI8ek0dQdx5M9civ8a+TA0zniCDLCNuLEstWeUGKtAuCw==';
+
+if (Dynamsoft && Dynamsoft.WebTwainEnv && Dynamsoft.WebTwainEnv.ProductKey)
+	dynamsoft.dbrEnv.productKey = [Dynamsoft.WebTwainEnv.ProductKey, dynamsoft.dbrEnv.productKey].join(";");
 
 // If you want to connect to the service manually, please set it to false
 // and call dynamsoft.BarcodeReader.initServiceConnection when needed.
@@ -38,13 +41,12 @@ dynamsoft.dbrEnv.bAutoConnectService = false;
 
 /// set resourcesPath in runtime is ok
 (function () {
-    var p = document.location.protocol;
-    if (p !== 'https:' && p !== 'http:')
-        dynamsoft.dbrEnv.resourcesPath = "https://tst.dynamsoft.com/libs/dbr/6.5.2";
-    else
-        dynamsoft.dbrEnv.resourcesPath = "http://tst.dynamsoft.com/libs/dbr/6.5.2";
+	var p = document.location.protocol;
+	if (p !== 'https:' && p !== 'http:')
+		dynamsoft.dbrEnv.resourcesPath = "https://tst.dynamsoft.com/libs/dbr/7.0";
+	else
+		dynamsoft.dbrEnv.resourcesPath = "http://tst.dynamsoft.com/libs/dbr/7.0";
 })();
-
 
 // try 64bit first, default false
 //dynamsoft.dbrEnv.ifCheck64bitServiceFirst = true;
